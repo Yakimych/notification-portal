e0d0060c82660cb45ecc8c8f77b6164c01b9aa07
Kyrylo Yakymenko <yakimych@gmail.com>
2020-10-13 15:23:05 +0200

    Set admin username and password when creating web app in Azure

--- infrastructure/AzureCreateAndPublish.ps1
+++ infrastructure/AzureCreateAndPublish.ps1
@@ -1,6 +1,14 @@
+param (
+  [string]$ADMIN_USERNAME,
+  [string]$ADMIN_PASSWORD
+)
+
+if (-not($ADMIN_USERNAME)) { Throw "ADMIN_USERNAME should be specified" }
+if (-not($ADMIN_PASSWORD)) { Throw "ADMIN_PASSWORD should be specified" }
+
 $PROJECT_FILE = "../src/NotificationPortal.Web/NotificationPortal.Web.csproj"
 $OUTPUT_FOLDER = "./publish_output"
 
 rm -rf $OUTPUT_FOLDER
 dotnet publish $PROJECT_FILE -c release -o $OUTPUT_FOLDER
-dotnet run -- $OUTPUT_FOLDER
+dotnet run -- $OUTPUT_FOLDER $ADMIN_USERNAME $ADMIN_PASSWORD
--- infrastructure/Program.fs
+++ infrastructure/Program.fs
@@ -6,7 +6,7 @@ let commandLineArgs = Environment.GetCommandLineArgs()
 
 match commandLineArgs |> List.ofArray with
 | [] | [_] -> Console.WriteLine("No command line argument specified.")
-| _applicationName :: publishedAppFolder :: _ ->
+| _applicationName :: publishedAppFolder :: adminUserName :: adminPassword :: _ ->
 
     let resourceGroupName = "NotificationPortalGroup"
     let webApplicationName = "RelogifyNotificationPortal"
@@ -14,6 +14,7 @@ match commandLineArgs |> List.ofArray with
     let myWebApp = webApp {
         name webApplicationName
         zip_deploy publishedAppFolder
+        settings [ ("admin_username", adminUserName); ("admin_password", adminPassword) ]
         app_insights_off
     }
 
@@ -25,3 +26,4 @@ match commandLineArgs |> List.ofArray with
     deployment
     |> Deploy.execute resourceGroupName Deploy.NoParameters
     |> ignore
+| _ -> Console.WriteLine("There should be exactly 3 command line argument specified: publishedAppFolder, adminUserName and adminPassword.")
