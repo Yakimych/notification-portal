50ee742b1e5834603442f7c89e83fc15ed452325
Kyrylo Yakymenko <yakimych@gmail.com>
2020-10-17 14:24:40 +0200

    Add logging to ApplicationInsights

--- src/NotificationPortal.Web/Controllers/ChallengeController.cs
+++ src/NotificationPortal.Web/Controllers/ChallengeController.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using Hangfire;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using NotificationPortal.Web.Core;
 using NotificationPortal.Web.Data;
 using NotificationPortal.Web.Models;
@@ -12,11 +13,14 @@ namespace NotificationPortal.Web.Controllers
     {
         private readonly ApplicationDbContext _dbContext;
         private readonly ChallengeService _challengeService;
+        private readonly ILogger<HomeController> _logger;
 
-        public ChallengeController(ApplicationDbContext dbContext, ChallengeService challengeService)
+        public ChallengeController(
+            ApplicationDbContext dbContext, ChallengeService challengeService, ILogger<HomeController> logger)
         {
             _dbContext = dbContext;
             _challengeService = challengeService;
+            _logger = logger;
         }
 
         [HttpGet]
@@ -28,6 +32,7 @@ namespace NotificationPortal.Web.Controllers
         [HttpGet]
         public IActionResult ChallengeList()
         {
+            _logger.Log(LogLevel.Information, "ChallengeList action called");
             return View(new ChallengeListViewModel());
         }
 
