9f188dce597063d2ea8d5a2f3c8bc1dcebde65e1
Kyrylo Yakymenko <yakimych@gmail.com>
2020-10-26 12:24:34 +0100

    Add webpack configuration with TypeScript support

---
+++ src/NotificationPortal.Web/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+// TODO: Remove
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/index.ts",
+  output: {
+    path: path.resolve(__dirname, "wwwroot/transpiled"),
+    filename: "[name].[chunkhash].js",
+    publicPath: "/",
+  },
+  resolve: {
+    extensions: [".js", ".ts"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "css/[name].[chunkhash].css",
+    }),
+  ],
+};
