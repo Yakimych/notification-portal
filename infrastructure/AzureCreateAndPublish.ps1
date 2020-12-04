5d260b64338a824cac46406f5e3ff67701a3de51
Kyrylo Yakymenko <yakimych@gmail.com>
2020-12-04 17:25:56 +0100

    Add npm commands to build/deployment script

--- infrastructure/AzureCreateAndPublish.ps1
+++ infrastructure/AzureCreateAndPublish.ps1
@@ -6,9 +6,18 @@ param (
 if (-not($ADMIN_USERNAME)) { Throw "ADMIN_USERNAME should be specified" }
 if (-not($ADMIN_PASSWORD)) { Throw "ADMIN_PASSWORD should be specified" }
 
-$PROJECT_FILE = "../src/NotificationPortal.Web/NotificationPortal.Web.csproj"
+$PROJECT_DIR = "../src/NotificationPortal.Web/"
+$PROJECT_FILE = "NotificationPortal.Web.csproj"
+$FULL_PROJECT_PATH = Join-Path -Path $PROJECT_DIR -ChildPath $PROJECT_FILE
+
 $OUTPUT_FOLDER = "./publish_output"
 
 rm -rf $OUTPUT_FOLDER
-dotnet publish $PROJECT_FILE -c release -o $OUTPUT_FOLDER
+
+Set-Location $PROJECT_DIR
+npm install
+npm run release
+Set-Location ../../infrastructure
+
+dotnet publish $FULL_PROJECT_PATH -c release -o $OUTPUT_FOLDER
 dotnet run -- $OUTPUT_FOLDER $ADMIN_USERNAME $ADMIN_PASSWORD
