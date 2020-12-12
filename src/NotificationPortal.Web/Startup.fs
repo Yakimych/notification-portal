8d38fb0678c7f0e10d076df1b4bf08c9601c0cac
Kyrylo Yakymenko <yakimych@gmail.com>
2020-12-12 16:15:09 +0100

    Add Swagger and Hangfire code to Startup

--- src/NotificationPortal.Web/Startup.fs
+++ src/NotificationPortal.Web/Startup.fs
@@ -1,34 +1,28 @@
 namespace NotificationPortal.Web
 
-open System
-open System.Collections.Generic
-open System.Linq
-open System.Threading.Tasks
 open Microsoft.AspNetCore.Builder
 open Microsoft.AspNetCore.Hosting
-open Microsoft.AspNetCore.HttpsPolicy;
-open Microsoft.AspNetCore.Mvc
 open Microsoft.Extensions.Configuration
 open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.Hosting
-
-
 open Hangfire
 open Hangfire.Dashboard
 open Hangfire.Storage.SQLite
-open Microsoft.AspNetCore.Builder
 open Microsoft.AspNetCore.Identity
-open Microsoft.AspNetCore.Hosting
 open Microsoft.EntityFrameworkCore
 open NotificationPortal.Data
-open Microsoft.Extensions.Configuration
-open Microsoft.Extensions.DependencyInjection
-open Microsoft.Extensions.Hosting
 //open NotificationPortal.Web.Core
 //open NotificationPortal.Web.Hubs
 open Microsoft.OpenApi.Models
 open System.Text.Json.Serialization
 
+type HangfireAuthorizationFilter() =
+    interface IDashboardAuthorizationFilter with
+        member this.Authorize (context: DashboardContext) =
+            let httpContext = context.GetHttpContext()
+
+            // TODO: Restrict to admin
+            httpContext.User.Identity.IsAuthenticated
 
 type Startup private () =
     new (configuration: IConfiguration) as this =
@@ -51,6 +45,20 @@ type Startup private () =
             .AddJsonOptions(fun options -> options.JsonSerializerOptions.Converters.Add(JsonStringEnumConverter()))
             .AddRazorRuntimeCompilation() |> ignore
 
+        services.AddSwaggerGen(fun c -> c.SwaggerDoc("v1", OpenApiInfo(Title = "NotificationPortal", Version = "v1"))) |> ignore
+        services.AddSignalR() |> ignore
+
+        services.AddHangfire(fun configuration ->
+            configuration
+                .UseSimpleAssemblyNameTypeSerializer()
+                .UseRecommendedSerializerSettings()
+                .UseSQLiteStorage()
+            |> ignore
+        ) |> ignore
+        services.AddHangfireServer() |> ignore
+//        services.AddScoped<IFirebaseMessagingService>()
+//        services.AddScoped<ChallengeService>()
+
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     member this.Configure(app: IApplicationBuilder, env: IWebHostEnvironment) =
 
@@ -61,18 +69,28 @@ type Startup private () =
             // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts() |> ignore
 
+        app.UseSwagger() |> ignore
+        app.UseSwaggerUI(fun c -> c.SwaggerEndpoint("/swagger/v1/swagger.json", "NotificationPortal API v1")) |> ignore
+
         app.UseHttpsRedirection() |> ignore
         app.UseStaticFiles() |> ignore
 
         app.UseRouting() |> ignore
+        app.UseDefaultFiles() |> ignore
 
         app.UseAuthentication() |> ignore
         app.UseAuthorization() |> ignore
+
+        app.UseHangfireDashboard("/hangfire", DashboardOptions(Authorization = [| HangfireAuthorizationFilter() |]))
+        |> ignore
 
         app.UseEndpoints(fun endpoints ->
             endpoints.MapControllerRoute(
                 name = "default",
                 pattern = "{controller=Home}/{action=Index}/{id?}") |> ignore
-            endpoints.MapRazorPages() |> ignore) |> ignore
+            endpoints.MapRazorPages() |> ignore
+
+//            endpoints.MapHub<ChallengeHub>("/challengehub");
+        ) |> ignore
 
     member val Configuration : IConfiguration = null with get, set
